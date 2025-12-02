4639081908f3c287e1bd1b84f6f2a709b3ba0b89
Yuriy Subbotin <Zencer1335@gmail.com>
2025-12-02 12:54:33 +0300

    add: method loadUserByUsername

--- src/main/java/com/forum/forum/service/UserService.java
+++ src/main/java/com/forum/forum/service/UserService.java
@@ -1,21 +1,38 @@
 package com.forum.forum.service;
 
+import com.forum.forum.model.Role;
 import com.forum.forum.model.User;
 import com.forum.forum.repository.user.UserRepository;
+import com.forum.forum.security.AuthorizedUser;
 import com.forum.forum.util.ValidUtil;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostConstruct
+    public void initUser() {
+        userRepository.save(new User(
+                "user@mail.com", "name",
+                passwordEncoder.encode("password"), Role.USER));
+    }
+
     public User createUser(User user) {
         ValidUtil.checkIsNew(user);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
@@ -36,5 +53,14 @@ public class UserService {
 
     public List<User> getAll() {
         return userRepository.getAll();
+    }
+
+    @Override
+    public AuthorizedUser loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.getByEmail(email.toLowerCase());
+        if (user == null) {
+            throw new UsernameNotFoundException("User " + email + " is not found");
+        }
+        return new AuthorizedUser(user);
     }
 }
