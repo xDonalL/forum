aeae5e1eabba3aca7c9f09e14e8326088f391767
Yuriy Subbotin <Zencer1335@gmail.com>
2025-12-02 12:53:12 +0300

    add: implements UserDetails

---
+++ src/main/java/com/forum/forum/security/AuthorizedUser.java
@@ -0,0 +1,63 @@
+package com.forum.forum.security;
+
+import com.forum.forum.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class AuthorizedUser implements UserDetails {
+
+    private final User user;
+
+    public AuthorizedUser(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.name()))
+                .toList();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isEnabled();
+    }
+
+    @Override
+    public String toString() {
+        return "SecurityUser{" + user.toString() + "}";
+    }
+}
