5cf6b3c6b75d57ddd2e3c89256907650517a6770
Yuriy Subbotin <Zencer1335@gmail.com>
2025-12-02 14:26:32 +0300

    add: UserController

---
+++ src/main/java/com/forum/forum/web/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.forum.forum.web.controller;
+
+import com.forum.forum.model.User;
+import com.forum.forum.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/profile")
+public class UserController {
+
+    @Autowired
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String profilePage(Model model, Authentication auth) {
+        User user = userService.getByEmail(auth.getName());
+        model.addAttribute("user", user);
+        return "user-profile";
+    }
+
+    @GetMapping("/users")
+    public String usersPage(Model model) {
+        model.addAttribute("users", userService.getAll());
+        return "users";
+    }
+}
