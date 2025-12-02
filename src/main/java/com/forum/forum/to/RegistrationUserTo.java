0194b6fc12aa0bdb09d087628d999b8022dcce76
Yuriy Subbotin <Zencer1335@gmail.com>
2025-12-02 18:01:10 +0300

    add: get/post register

---
+++ src/main/java/com/forum/forum/to/RegistrationUserTo.java
@@ -0,0 +1,13 @@
+package com.forum.forum.to;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegistrationUserTo {
+    private String email;
+    private String name;
+    private String password;
+    private String confirmPassword;
+}
--- src/main/java/com/forum/forum/web/controller/RootController.java
+++ src/main/java/com/forum/forum/web/controller/RootController.java
@@ -1,16 +1,45 @@
 package com.forum.forum.web.controller;
 
+import com.forum.forum.service.UserService;
+import com.forum.forum.to.RegistrationUserTo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class RootController {
 
+    @Autowired
+    private final UserService userService;
+
+    public RootController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping
     public String usersPage(Model model) {
         return "home-page";
+    }
+
+    @GetMapping("/register")
+    public String registerPage(Model model) {
+        model.addAttribute("registrationTo", new RegistrationUserTo());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String register(@ModelAttribute RegistrationUserTo registrationTo, Model model) {
+        try {
+            userService.register(registrationTo);
+            return "redirect:/login";
+        } catch (IllegalArgumentException ex) {
+            model.addAttribute("error", ex.getMessage());
+            return "register";
+        }
     }
 }
