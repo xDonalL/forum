a59c2a573c55293a1e2ffed214b36ec0d1457ce6
Yuriy Subbotin <Zencer1335@gmail.com>
2025-12-02 14:50:35 +0300

    add: AdminController

---
+++ src/main/java/com/forum/forum/web/controller/AdminController.java
@@ -0,0 +1,26 @@
+package com.forum.forum.web.controller;
+
+import com.forum.forum.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @Autowired
+    private final UserService userService;
+
+    public AdminController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public String usersPage(Model model) {
+        model.addAttribute("users", userService.getAll());
+        return "users";
+    }
+}
