e08be4a654ef30cefc96db33cd84fe98127ce33e
Yuriy Subbotin <Zencer1335@gmail.com>
2025-12-04 03:16:51 +0300

    add: ForumTopic

---
+++ src/main/java/com/forum/forum/model/ForumTopic.java
@@ -0,0 +1,32 @@
+package com.forum.forum.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "forum_topics")
+public class ForumTopic extends AbstractBaseEntity {
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    @Column(name = "date_created", nullable = false, updatable = false)
+    private final LocalDateTime dateCreated = LocalDateTime.now();
+
+    @OneToMany(mappedBy = "topic", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final List<ForumComment> messages = new ArrayList<>();
+}
