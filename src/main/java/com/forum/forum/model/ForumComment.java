b5989b9d78b62aead67a655825b6331aa6edb39a
Yuriy Subbotin <Zencer1335@gmail.com>
2025-12-04 03:17:11 +0300

    add: ForumComment

---
+++ src/main/java/com/forum/forum/model/ForumComment.java
@@ -0,0 +1,28 @@
+package com.forum.forum.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "forum_comment")
+public class ForumComment extends AbstractBaseEntity {
+
+    @Column(name = "comment", nullable = false, columnDefinition = "TEXT")
+    private String comment;
+
+    @Column(name = "date_created", nullable = false, updatable = false)
+    private final LocalDateTime dateCreated = LocalDateTime.now();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "topic_id", nullable = false)
+    private ForumTopic topic;
+}
