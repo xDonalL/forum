3feef806d53bc76e2d0a5e0c44602134510f5f70
Yuriy Subbotin <Zencer1335@gmail.com>
2025-12-24 23:40:44 +0300

    add/rename: test method for AdminController

--- src/test/java/com/forum/forum/web/controller/AdminControllerTest.java
+++ src/test/java/com/forum/forum/web/controller/AdminControllerTest.java
@@ -15,9 +15,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import static com.forum.forum.UserTestData.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(AdminController.class)
@@ -31,7 +31,7 @@ class AdminControllerTest {
     private UserService userService;
 
     @Test
-    void usersPage() throws Exception {
+    void getShowUsersPanel_whenAdmin_thenIsOk() throws Exception {
         when(userService.filterUsers(null)).thenReturn(ALL_USERS);
 
         AuthorizedUser authAdmin = new AuthorizedUser(ADMIN);
@@ -48,10 +48,63 @@ class AdminControllerTest {
     }
 
     @Test
-    void usersPageForbiddenForUser() throws Exception {
+    void getShowUsersPanel_whenUser_thenForbidden() throws Exception {
         mockMvc.perform(get("/admin/panel")
-                        .with(user(USER.getEmail())
-                                .roles(String.valueOf(Role.USER))))
+                        .with(user(USER.getEmail()).roles(String.valueOf(Role.USER))))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void getShowUsersPanel_whenNotAuthorized_thenRedirectLogin() throws Exception {
+        mockMvc.perform(get("/admin/panel"))
+                .andExpect(redirectedUrlPattern("**/login"));
+    }
+
+    @Test
+    void postBanUser_whenAdmin_thenSuccess() throws Exception {
+        mockMvc.perform(post("/admin/ban/" + USER_ID)
+                        .with(user(ADMIN.getEmail()).roles(String.valueOf(Role.ADMIN))))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/admin/panel"));
+
+        verify(userService).banUser(USER_ID);
+    }
+
+    @Test
+    void postBanUser_whenUser_thenForbidden() throws Exception {
+        mockMvc.perform(post("/admin/ban/" + USER_ID)
+                        .with(user(USER.getEmail()).roles(String.valueOf(Role.USER))))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void postBanUser_whenModer_thenForbidden() throws Exception {
+        mockMvc.perform(post("/admin/ban/" + USER_ID)
+                        .with(user(MODER.getEmail()).roles(String.valueOf(Role.MODERATOR))))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void postUnbanUser_whenAdmin_thenSuccess() throws Exception {
+        mockMvc.perform(post("/admin/unban/" + USER_ID)
+                        .with(user(ADMIN.getEmail()).roles(String.valueOf(Role.ADMIN))))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/admin/panel"));
+
+        verify(userService).unbanUser(USER_ID);
+    }
+
+    @Test
+    void postUnbanUser_whenUser_thenForbidden() throws Exception {
+        mockMvc.perform(post("/admin/unban/" + USER_ID)
+                        .with(user(USER.getEmail()).roles(String.valueOf(Role.USER))))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void postUnbanUser_whenModer_thenForbidden() throws Exception {
+        mockMvc.perform(post("/admin/unban/" + USER_ID)
+                        .with(user(MODER.getEmail()).roles(String.valueOf(Role.MODERATOR))))
                 .andExpect(status().isForbidden());
     }
 }
