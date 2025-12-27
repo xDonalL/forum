af203456ce10ea4008b57809d88c0c0c3f3071a3
Yuriy Subbotin <Zencer1335@gmail.com>
2025-12-27 23:29:12 +0300

    add: log for ValidUtil

--- src/main/java/com/forum/forum/util/ValidUtil.java
+++ src/main/java/com/forum/forum/util/ValidUtil.java
@@ -1,9 +1,14 @@
 package com.forum.forum.util;
 
-import com.forum.forum.util.exception.NotFoundException;
 import com.forum.forum.model.AbstractBaseEntity;
+import com.forum.forum.util.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ValidUtil {
+
+    public static final Logger log = LoggerFactory.getLogger(ValidUtil.class);
+
     public static void checkNotFound(boolean found, int id) {
         checkNotFound(found, "id=" + id);
     }
@@ -15,6 +20,7 @@ public class ValidUtil {
 
     public static boolean checkNotFound(boolean found, String msg) {
         if (!found) {
+            log.warn("Entity not found: {}", msg);
             throw new NotFoundException("Not found entity with " + msg);
         }
         return true;
@@ -22,6 +28,8 @@ public class ValidUtil {
 
     public static void checkIsNew(AbstractBaseEntity bean) {
         if (!bean.isNew()) {
+            log.warn("Entity must be new (id=null), but was id={}",
+                    bean.getId());
             throw new IllegalArgumentException(bean + " must be new (id=null)");
         }
     }
