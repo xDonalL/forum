4fc2848155a08ef440fbd6381e1279e263721ce5
Yuriy Subbotin <Zencer1335@gmail.com>
2025-12-30 00:31:28 +0300

    update: method isOwner add exception

--- src/main/java/com/forum/forum/security/TopicCommentSecurity.java
+++ src/main/java/com/forum/forum/security/TopicCommentSecurity.java
@@ -4,8 +4,12 @@ import com.forum.forum.model.TopicComment;
 import com.forum.forum.model.User;
 import com.forum.forum.repository.forum.DataJpaTopicCommentRepository;
 import com.forum.forum.service.UserService;
+import com.forum.forum.util.exception.NotFoundException;
+import com.forum.forum.util.exception.AuthenticationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.nio.file.AccessDeniedException;
 
 @Component("commentSecurity")
 @RequiredArgsConstructor
@@ -13,9 +17,21 @@ public class TopicCommentSecurity {
     private final DataJpaTopicCommentRepository commentRepository;
     private final UserService userService;
 
-    public boolean isOwner(Integer commentId) {
+    public boolean isOwner(Integer commentId) throws Exception{
         User user = userService.getCurrentUser();
+        if (user == null) {
+            throw new AuthenticationException("User is not authenticated");
+        }
+
         TopicComment comment = commentRepository.get(commentId);
-        return comment != null && comment.getAuthor().getId().equals(user.getId());
+
+        if (comment == null) {
+            throw new NotFoundException("Comment not found");
+        }
+
+        if (!comment.getAuthor().getId().equals(user.getId())) {
+            throw new AccessDeniedException("You are not owner of this comment");
+        }
+        return true;
     }
 }
--- src/main/java/com/forum/forum/security/TopicSecurity.java
+++ src/main/java/com/forum/forum/security/TopicSecurity.java
@@ -4,8 +4,12 @@ import com.forum.forum.model.Topic;
 import com.forum.forum.model.User;
 import com.forum.forum.repository.forum.DataJpaTopicRepository;
 import com.forum.forum.service.UserService;
+import com.forum.forum.util.exception.NotFoundException;
+import com.forum.forum.util.exception.AuthenticationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.nio.file.AccessDeniedException;
 
 @Component("topicSecurity")
 @RequiredArgsConstructor
@@ -13,9 +17,21 @@ public class TopicSecurity {
     private final DataJpaTopicRepository topicRepository;
     private final UserService userService;
 
-    public boolean isOwner(Integer topicId) {
+    public boolean isOwner(Integer topicId) throws Exception {
         User user = userService.getCurrentUser();
-        Topic comment = topicRepository.get(topicId);
-        return comment != null && comment.getAuthor().getId().equals(user.getId());
+        if (user == null) {
+            throw new AuthenticationException("User is not authenticated");
+        }
+
+        Topic topic = topicRepository.get(topicId);
+
+        if (topic == null) {
+            throw new NotFoundException("Topic not found");
+        }
+
+        if (!topic.getAuthor().getId().equals(user.getId())) {
+            throw new AccessDeniedException("You are not owner of this comment");
+        }
+        return true;
     }
 }
